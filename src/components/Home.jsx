3e8a35e9955cbd2fdf3efc9c4fa5a1ba8e94f9e3
Vikalp Mishra <mishravikalp896@gmail.com>
2023-10-12 19:02:50 +0530

    working on home

---
+++ src/components/Home.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import Typerwriter from 'typewriter-effect'
+import { BsArrowUpRight } from 'react-icons/bs'
+export default function Home() {
+    const animation = {
+        h1: {
+            initial: {
+                x: '-100%',
+                opacity: '0'
+            },
+            whileInView: {
+                x: '0',
+                opacity: '1'
+            }
+        },
+        button: {
+            initial: {
+                y: '-100%',
+                opacity: '0'
+            },
+            whileInView: {
+                y: '0',
+                opacity: '1'
+            }
+        }
+    }
+    return (
+        <div id='home'>
+            <section>
+                <div>
+                    <motion.h1 {...animation.h1}>
+                        Hi, I'm  <br /> Vikalp Mishra
+                    </motion.h1>
+
+                    <Typerwriter options={{ strings: ['A Developer', 'A Designer', 'A Deep Learner Engineer', 'MERN Stack Developer'], autoStart: true, loop: true, wrapperClassName: 'typewriterpara', cursor: '' }} />
+                    <div>
+                        <a href="mailto:mishravikalp896@gmail.com">Hire Me</a>
+                        <a href="#work">Project <BsArrowUpRight /></a>
+                    </div>
+                    <article data-special>
+                        <p>Contact</p>
+                        <span>mishravikalp896@gmail.com</span>
+                    </article>
+                </div>
+            </section>
+            <section></section>
+        </div>
+    )
+}
