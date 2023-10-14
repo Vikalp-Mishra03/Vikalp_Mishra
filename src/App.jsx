c600b52ddf80b9104a212467c572484a5c2179ce
Vikalp Mishra <mishravikalp896@gmail.com>
2023-10-14 09:26:38 +0530

    changes in file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from "react"
 import Header from "./components/Header"
+import Home from "./components/Home"
+import About from "./components/About"
+import Days from "./components/Days"
+import Work from "./components/Work"
+// import Contact from "./components/Contact"
+// import { Toaster } from "react-hot-toast"
+import Footer from './components/Footer'
 function App() {
- return (
-    <Header/>
+  return (
+    <>
+      <Header />
+      <Home />
+      <About />
+      <Days />
+      <Work />
+      {/* <Contact /> */}
+      {/* <Toaster /> */}
+      <Footer />
+    </>
   )
 }
 
